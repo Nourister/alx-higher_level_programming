0d0a6d7982b62eee69186f9ece50b933a9f9e6d4
nourister <nouristerjuma@gmail.com>
2024-01-06 02:50:04 -0800

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,8 @@
 """
  a Python script that fetches https://alx-intranet.hbtn.io/status
  """
- import requests
- if __name__ == "__main__":
+import requests
+if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     content = response.text
